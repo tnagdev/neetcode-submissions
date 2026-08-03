579c94174dedccb1bc9119471628ab06e8b8af9c
tnagdev <57598793+tnagdev@users.noreply.github.com>
2026-08-03 20:38:55 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const set = new Set(nums);
+        let max = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (set.has(nums[i]) && set.has(nums[i] - 1)) continue;
+            let start = nums[i]
+            while (set.has(start)) start++;
+            max = Math.max(max, start - nums[i]);
+        }
+        return max;
+    }
+}
